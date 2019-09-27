01d8883b014150998ed0c9cbf2e29fa42e343752
vysoki <vysoki@gmail.com>
2019-09-27 12:32:38 +0000

    Deploying to gh-pages from master d8cac676ccbb889f2ed21b198f6213c9f700c765

---
+++ precache-manifest.d3c4c439a5c1eeabab7fcd9ab6af6721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d1611771f528093b9df445c6498fb7",
+    "url": "/sample-app/index.html"
+  },
+  {
+    "revision": "1780a6688944f458a716",
+    "url": "/sample-app/static/js/2.e71a02f1.chunk.js"
+  },
+  {
+    "revision": "0aadb7b0942dcd87006f",
+    "url": "/sample-app/static/js/main.e2baa0f3.chunk.js"
+  },
+  {
+    "revision": "a889d3666879c52e6b15",
+    "url": "/sample-app/static/js/runtime-main.4fe6d463.js"
+  },
+  {
+    "revision": "d6212e54888ede059ad2defb174039a4",
+    "url": "/sample-app/static/media/cave.d6212e54.jpg"
+  },
+  {
+    "revision": "abc3a6da693f3a9b8260d73588259fca",
+    "url": "/sample-app/static/media/logo.abc3a6da.png"
+  }
+]);
